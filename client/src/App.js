16ceff229fdbb8e9edb187151a6828e180e9828d
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-12-09 22:25:57 +0530

    react-leaflet setup added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-import logo from "./logo.svg";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import "./App.css";
 
 function App() {
+  const center = [28.593706674129685, 77.17769044788487];
+
   return (
-    <div className="App">
-      <h1>Welcome to Roam.ai</h1>
-    </div>
+    <>
+      <MapContainer
+        style={{ width: "100vw", height: "100vh" }}
+        center={center}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
   );
 }
 
